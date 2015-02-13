e1013c96194d9ddc862a75517e45db37273a0825
Luis Vega <luis@sting.local>
2015-02-13 09:28:35 +0800

    Removed uneeded method

--- cuba/handler.go
+++ cuba/handler.go
@@ -1,20 +1,6 @@
 package cuba
 
-import (
-	"log"
-	"net/http"
-)
-
 type Handler func(*Context) error
-
-func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	context := &Context{w, r, make(map[string]string), r.URL.Path}
-
-	err := h.serveContext(context)
-	if err != nil {
-		log.Println(err)
-	}
-}
 
 func (h Handler) serveContext(context *Context) error {
 	err := h(context)
